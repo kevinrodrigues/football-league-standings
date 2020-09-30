f7477896f81bfa96b9aba3e3d1b4c34578482831
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2020-09-30 12:46:02 +0100

    VTF-2 - 29/09/2020 results.

---
+++ src/components/LeagueSitePrices.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <modal name="league-site-prices" height="auto" :adaptive="true" :scrollable="true">
+      <h2>League table pricing</h2>
+      £250
+      <button>See Prices</button>
+      <button @click="onDismissModal">Close</button>
+    </modal>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import { setCookie } from '../utils/helpers';
+
+export default {
+  methods: {
+    ...mapActions([
+      'hasDismissedPricesModal',
+    ]),
+
+    onDismissModal() {
+      this.hasDismissedPricesModal(true);
+      setCookie('league-prices-dismissed', true);
+    },
+  },
+};
+</script>
