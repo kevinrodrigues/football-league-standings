9be555362bab71c8b0784fa5c513c671c983e916
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2021-04-03 23:53:24 +0100

    VTF - 01-04-2021 - standings.

--- src/views/LeagueTable.vue
+++ src/views/LeagueTable.vue
@@ -24,7 +24,7 @@
 
     <h3>{{ getSwitchedStateHeading }}</h3>
     <!-- TODO: pull from build config -->
-    <p class="font-small">Last updated: <strong>18/12/2020 at 17:40</strong></p>
+    <p class="font-small">Last updated: <strong>03/04/2021 at 23:51</strong></p>
 
     <div v-if="isFilterOverlayOpen" class="search-overlay block">
       <h3>Filter table by player</h3>
